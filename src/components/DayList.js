a9e246ad91210f1d8beebb17e740e971d0b138f4
adrianetodesign <adrianetodesign@gmail.com>
2022-02-22 14:50:27 -0800

    Add DayListItem array to populate DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,9 +2,14 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  return (
-    <ul>
-
-    </ul>
-  );
+  const listDays = props.days.map(day => {
+    return (
+    <DayListItem
+      key={day.id}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+      {...day}
+    />);
+  });
+  return <ul>{listDays}</ul>;
 };
