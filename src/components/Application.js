33b328e94cd827b055bcf73d9192854291a5e421
adrianetodesign <adrianetodesign@gmail.com>
2022-02-24 15:57:23 -0800

    Add Promise.all to useEffect to perform multiple axios.get requests

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,14 +20,14 @@ export default function Application() {
   })
   const setDay = day => setState({ ...state, day });
   
-  const setDays = (days) => {
-    setState(prev => ({ ...prev, days }));
-  };
-  
   useEffect(() => {
-    axios.get(`/api/days`).then(res => {
-      setDays(res.data)
-  }).catch(err => console.log(err));
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/interviewers'),
+      axios.get('/api/appointments')
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    });
 }, []);
 
   return (
