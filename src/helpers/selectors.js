e7872da865bfb33dadd7bec6148c7eef21d72f2b
adrianetodesign <adrianetodesign@gmail.com>
2022-02-24 15:28:56 -0800

    Add getAppointmentsForDay function implementation

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,10 @@
 export function getAppointmentsForDay(state, day) {
+  const appointIdList = [];
+  state.days.map(dayItem => {
+    if (dayItem.name === day) {
+      dayItem.appointments.forEach(id => appointIdList.push(id))
+    }
+  })
 
+  return appointIdList.map(id => state.appointments[id]);
 }
