e195a32c14e81c6b4d93ab7369d55917ba737012
adrianetodesign <adrianetodesign@gmail.com>
2022-02-24 14:54:31 -0800

    Refactor Application component with useEffect and axios get request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import "components/Application.scss";
 
 const appointments = [
   {
@@ -67,8 +49,17 @@ const appointmentList = appointments.map(appointment => {
 })
 
 
-export default function Application(props) {
+export default function Application() {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/days`).then(res => {
+      setDay(res.data)
+      setDays(res.data)
+  }).catch(err => console.log(err));
+}, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
