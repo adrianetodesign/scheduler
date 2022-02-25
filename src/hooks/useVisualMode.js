2f435f9f32c7e6648eadeae726fd520aff77f134
adrianetodesign <adrianetodesign@gmail.com>
2022-02-25 13:22:28 -0800

    Add history and sethistory to useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,8 +2,10 @@ import { useState } from 'react';
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode])
 
   function transition(newMode) {
+    setHistory(prev => [...prev, newMode]);
     setMode(newMode);
   }
 
