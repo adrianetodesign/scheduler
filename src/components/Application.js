eb7e0a229691628d68feca07790e461589655930
adrianetodesign <adrianetodesign@gmail.com>
2022-02-24 16:02:21 -0800

    Add import for getAppointmentsForDay func from  helpers/selectors.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application() {
   const [state, setState] = useState({
@@ -13,7 +14,7 @@ export default function Application() {
     appointments: {}
   });
   
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   
   const appointmentList = dailyAppointments.map(appointment => {
     return <Appointment key={appointment.id} {...appointment} />
@@ -22,9 +23,9 @@ export default function Application() {
   
   useEffect(() => {
     Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/interviewers'),
-      axios.get('/api/appointments')
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("api/interviewers")
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     });
