4da8df00a0af9809ebecb29fcb5b226a43612e83
adrianetodesign <adrianetodesign@gmail.com>
2022-02-25 13:01:29 -0800

    Add getInterview helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,7 +4,19 @@ export function getAppointmentsForDay(state, day) {
     if (dayItem.name === day) {
       dayItem.appointments.forEach(id => appointIdList.push(id))
     }
+    return null;
   })
 
   return appointIdList.map(id => state.appointments[id]);
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewerData = state.interviewers[interview.interviewer];
+  return {
+    student: interview.student,
+    interviewer: interviewerData
+  }
 }
