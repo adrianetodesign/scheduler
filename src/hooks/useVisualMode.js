b97000b565f08c4071a1db5c6acf3accb29383c7
adrianetodesign <adrianetodesign@gmail.com>
2022-02-25 16:14:02 -0800

    Add back function to useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,12 +4,19 @@ export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode])
 
-  function transition(newMode) {
+  const transition = (newMode) => {
     setHistory(prev => [...prev, newMode]);
     setMode(newMode);
-  }
+  };
 
-  function back() { /* ... */ }
+  const back = () => {
+    const prevHistory = history.slice(0, -1);
+    setHistory(prevHistory);
+    if (history.length > 1) {
+      setMode(prevHistory[prevHistory.length - 1]);
+    }
+  };
+
   
   return { mode, transition, back };
-}
+};
